b64234ae7686f3c7e19e830c62272e04f84a546c
otuk <otuk@kodeten.com>
2017-06-05 19:44:25 -0400

    initial commit

--- permuter.py
+++ permuter.py
@@ -1,63 +1,65 @@
 import math
 
 
-def findRepeats(word):
-    d = {}
-    d.update(zip(word,[0]*len(word)))
-    for a in word:
-        d[a] += 1
-    return d
+class Permutator:
+    """
+    class that finds possible permutations of a word
+
+    """
+
+    def __init__(self, iterable):
+        self._iterable = iterable
+        self._len = len(self._iterable)
+        self._d = self.findRepeats(self._iterable)
+
+    @staticmethod
+    def findRepeats(iterable):
+        d = {}
+        d.update(zip(iterable, [0]*len(iterable)))
+        for a in iterable:
+            d[a] += 1
+        return d
 
 
-def numberOfPerms(word):
-    l = len(word)
-    perms = math.factorial(l)
-    d = findRepeats(word)
-    for _, v in d.items():
-        perms /= math.factorial(v)
-    return perms, d
+    def numberOfPerms(self):
+        perms = math.factorial(self._len)
+        for v in self._d.values():
+            perms /= math.factorial(v)
+        return perms
 
+    @staticmethod
+    def _permutations(d):
+        s = sum(d.values())
+        ls = set()
+        if len(d) == 1:
+            p1 = ""
+            for k, v in d.items():
+                for i in range(v):
+                    p1 += k
+            ls.add(p1)
+            return ls
+        elif s == 2:
+            p1, p2 = "",  ""
+            for k in d.keys():
+                p1 = p1 + k
+                p2 = k + p2
+            ls.add(p1)
+            ls.add(p2)
+            return ls
+        else:
+            for k, v in d.items():
+                sd = d.copy()
+                if v == 1:
+                    del sd[k]
+                else:
+                    sd[k] -= 1
+                l = Permutator._permutations(sd)
+                for w in l:
+                    ls.add(k + w)
+            return ls
 
-def allPerms(d):
-    s = sum(d.values())
-    print(s, d)
-    ls = set()
-    if len(d) == 1:
-        p1 = ""
-        for k, v in d.items():
-            for i in range(v):
-                p1 += k
-        ls.add(p1)
-        return ls
-    elif s == 2:
-        p1, p2 = "",  ""
-        for k in d.keys():
-            p1 = p1 + k
-            p2 = k + p2
-        ls.add(p1)
-        ls.add(p2)
-        return ls
-    else:
-        for k, v in d.items():
-            sd = d.copy()
-            if v == 1:
-                del sd[k]
-            else:
-                sd[k] -= 1
-            l = allPerms(sd)
-            for w in l:
-                ls.add(k + w)
-        return ls
-    print("unexpected condition")
+    def listPermutations(self):
+        return Permutator._permutations(self._d)
 
-
-
-if __name__ == "__main__":
-    word = "eatcu"
-    n, d = numberOfPerms(word)
-    print("Possible permutations " +str(n))
-    list = allPerms(d)
-    print("Perms expected vs actual: ", n," / ", len(list))
-    print("List of Perms: ", list)
 
 
